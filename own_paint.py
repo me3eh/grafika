7c8d2bbc1c299d6f66f701890b89a4aa013a8bdb
me3eh <matt30002000@gmail.com>
2022-10-10 02:00:37 +0200

    Added scaling, but now scaling by the point

--- own_paint.py
+++ own_paint.py
@@ -2,11 +2,31 @@ from tkinter import *
 from tkinter.colorchooser import askcolor
 import math
 import json
+import sys
 
 from shapes.line import Line
 from shapes.circle import Circle
 from shapes.rectangle import Rectangle
 from shapes.triangle import Triangle
+def pairwise(iterable):
+    a = iter(iterable)
+    return zip(a, a)
+
+def find_closest_point(main_point_x, main_point_y, array_of_points):
+    local_max = sys.maxsize
+    index_min = 0
+    for index in range(0, len(array_of_points), 2):
+        x = array_of_points[index]
+        y = array_of_points[index + 1]
+        distance_between = math.sqrt(math.pow(x - main_point_x, 2) + math.pow(y - main_point_y, 2))
+        if distance_between <= local_max:
+            local_max = distance_between
+            index_min = index
+    if len(array_of_points) == 0:
+        return array_of_points[index_min]
+
+    return (array_of_points[index_min], array_of_points[index_min + 1], index_min)
+
 
 class Paint(object):
 
@@ -41,11 +61,32 @@ class Paint(object):
         self.moving = Button(self.root, text='move', command=self.move)
         self.moving.grid(row=0, column=5)
 
-        self.choose_size_button = Scale(self.root, from_=1, to=10, orient=HORIZONTAL)
-        self.choose_size_button.grid(row=0, column=6)
+        self.scaling = Button(self.root, text='scale', command=self.scale)
+        self.scaling.grid(row=0, column=6)
 
-        self.c = Canvas(self.root, bg='white', width=1000, height=1000)
-        self.c.grid(row=1, columnspan=5)
+        self.choose_size_button = Scale(self.root, from_=1, to=10, orient=HORIZONTAL)
+        self.choose_size_button.grid(row=0, column=7)
+
+        self.c = Canvas(self.root, bg='white', width=600, height=600)
+        self.c.grid(row=1, rowspan=2, column=0, columnspan=10)
+
+        self.label = Label(self.root, text="First x")
+        self.label.grid(row=0, rowspan=2,  column=11)
+
+        self.inputing = Entry(self.root)
+        self.inputing.grid(row=1, column=11)
+
+        self.second_label = Label(self.root, text="First y")
+        self.second_label.grid(row=0, rowspan=2,  column=13)
+
+        self.second_inputing = Entry(self.root)
+        self.second_inputing.grid(row=1, column=13)
+
+        self.saving = Button(self.root, text='Save')
+        self.saving.grid(row=1, column=14)
+
+        self.c.selected = None
+        self.c.dot = None
 
         self.setup()
         self.root.mainloop()
@@ -110,7 +151,13 @@ class Paint(object):
         self.choice = 'move'
         self.stop_preview()
 
+    def scale(self):
+        self.activate_button(self.scaling)
+        self.choice = 'scale'
+        self.stop_preview()
+
     def paint(self, event):
+
         self.line_width = self.choose_size_button.get()
         paint_color = self.color
         self.click_x = event.x
@@ -143,39 +190,52 @@ class Paint(object):
                 self.first = False
 
         # print(self.choice)
-        if self.choice == 'move':
+        if self.choice in ['move', 'scale']:
             self.on_click(event)
         else:
             self.selected = None
         self.c.focus_set()
 
     def on_click(self, event):
-        # print('yasssss')
-
         self.selected = self.c.find_overlapping(event.x - 10, event.y - 10, event.x + 10, event.y + 10)
         if self.selected:
             self.c.selected = self.selected[-1]  # select the top-most item
             self.c.startxy = (event.x, event.y)
-            # print(self.c.selected, self.c.startxy)
         else:
             self.c.selected = None
 
     def on_drag(self, event):
-        if self.selected:
-            # calculate distance moved from last position
-            dx, dy = event.x - self.c.startxy[0], event.y - self.c.startxy[1]
-            # move the selected item
-            self.c.move(self.selected, dx, dy)
-            # update last position
-            self.c.startxy = (event.x, event.y)
+        if self.choice == 'move':
+            if self.c.selected:
+                dx, dy = event.x - self.c.startxy[0], event.y - self.c.startxy[1]
+                self.c.move(self.c.selected, dx, dy)
+                self.c.startxy = (event.x, event.y)
+        if self.choice == 'scale':
+            if self.c.selected:
+                coords = self.c.coords(self.c.selected)
+                print('ilosc', len(coords))
+                print(coords)
+                coords_of_point = self.c.coords(self.c.dot)
+                x, c, ll = find_closest_point(coords_of_point[0], coords_of_point[1], coords)
+                number_of_coords = [a for a in range(int(len(coords) / 2))]
+                index_of_closest_point = number_of_coords.index(ll)
+                del number_of_coords[index_of_closest_point]
+                array_for_scaling = []
+                for point in number_of_coords:
+                        array_for_scaling.append(coords[point])
+                        array_for_scaling.append(coords[point + 1])
+                array_for_scaling.append(event.x)
+                array_for_scaling.append(event.y)
+                self.c.coords(self.selected, array_for_scaling)
+                # self.c.delete(self.dot)
 
     def motion(self, event):
+        if self.c.dot:
+            self.c.delete(self.c.dot)
         if self.previews != []:
             for preview in self.previews:
                 self.c.delete(preview)
             self.previews.clear()
-        # print(type(self.choice2))
-        # print(self.choice2.type)
         if self.choice == 'triangle':
             if not self.first:
                 if self.doing:
@@ -186,10 +246,19 @@ class Paint(object):
             if self.second:
                 self.previews.append(Line().create(self.c, self.second_x, self.second_y, event.x, event.y,
                                              fill=self.color))
+        elif self.choice == 'scale':
+            if self.c.selected:
+                coords = self.c.coords(self.c.selected)
+                point = find_closest_point(event.x, event.y, coords)
+                self.c.dot = self.c.create_oval(point[0], point[1], point[0]+5, point[1]+5, fill='#ff0000')
+                # breakpoint()
+                # coords = self.c.coords(self.c.selected)
+                # event.x event.y
         else:
             if self.first:
                 self.previews.append(self.choice2.create(self.c, self.first_x, self.first_y, event.x, event.y,
                                                          fill=self.color))
+
         self.c.focus_set()
 
 
